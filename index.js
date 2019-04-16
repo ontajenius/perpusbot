0b73c688e00954de87bd7da543d8ac5df4f4958b
Ghif Aliy <ghifari.aliy@nsi.co.id>
2019-04-16 13:22:59 +0700

    - adding some regex

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 require('dotenv').config()
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const { Discord, RichEmbed, Client } = require('discord.js')
+const client = new Client()
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!')
+  let perpusRegex = /(\/perpus\s)(\S*)/m
+  let swearRegex = /(bangsat|bgzd|asu|goblog|gobs|anj)/m
+
+  if(swearRegex.test(msg.content)) {
+    msg.reply('Jangan kasar gitu dong sayang :pepesad:')
+  }
+
+  if(perpusRegex.test(msg.content)) {
+    let regexResult = perpusRegex.exec(msg.content)
+    let videoCode = regexResult[2]
+
+    let embed = new RichEmbed()
+      .setTitle(videoCode)
+      .setColor(0xFF0000)
+      .setDescription('Search result for ' + videoCode)
+      .setURL('https://www.google.com/search?q=' + videoCode)
+    msg.channel.send(embed)
   }
 })
 client.login(process.env.TOKEN)
