4f644781243306ae5fb04e4955a1fc6033b20462
GHIF-PC\Ghif <ontajenius@gmail.com>
2021-02-15 22:19:10 +0700

    Add birthday & insult funtion

--- menangos.js
+++ menangos.js
@@ -1,6 +1,13 @@
 require("dotenv").config();
-const { Client, RichEmbed } = require("discord.js");
-const client = new Client();
+const cron = require('node-cron');
+const { format } = require("date-fns");
+const { Client, MessageEmbed, Intents } = require("discord.js");
+const intents = new Intents(Intents.NON_PRIVILEGED);
+intents.add('GUILD_MEMBERS', 'GUILD_MESSAGES');
+const client = new Client({ ws: { intents: intents } });
+
+const birthdayList = require('./birthday.json');
+
 const _badWord = [
   "Anjing",
   "Babi",
@@ -84,19 +91,33 @@ const _badWord = [
 ];
 const _wotaWord = ["Jkt48", "jeketi", "jekate", "anin", "jkt", "theater"];
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+client.on("ready", async () => {
+  console.log(`Logged in as ${client.user?.tag}!`);
+  sendBirthday();
 });
+
+const sendBirthday = async () => {
+  const generalChannel = client.channels.cache.find((x) => x.id === '695217479139590180');
+
+  const today = format(new Date(), 'dd-MM');
+  const birthdaysToday = birthdayList.filter((data) => data.birthday === today);
+  if (birthdaysToday.length > 0) {
+    for (const birthday of birthdaysToday) {
+      generalChannel.send(`HBD ya sayangku <@${birthday.id}>, semoga panjang umur & sehat selalu :birthday: :partying_face:`);
+      generalChannel.send(`Kecup basah dari Ara buat <@${birthday.id}> :kissing_closed_eyes:`);
+    }
+  }
+}
 
 client.on("messageDelete", (msg) => {
   try {
-    const id = msg.author.id;
+    const id = msg.author?.id;
     msg.channel.send(`Hayo <@${id}> kamu hapus apaan?`);
     if (msg.attachments.size > 0) {
       const attachments = msg.attachments.array();
       for (const attachment of attachments) {
-        const repost = new RichEmbed()
-          .setAuthor(msg.author.tag, msg.author.displayAvatarURL)
+        const repost = new MessageEmbed()
+          .setAuthor(msg.author?.tag, msg.author?.displayAvatarURL())
           .setImage(attachment.url)
           .setTimestamp()
           .setFooter("Foto yang dihapus")
@@ -107,13 +128,12 @@ client.on("messageDelete", (msg) => {
       msg.channel.send(`Yang dihapus: ${msg.content}`);
     }
   } catch (error) {
-    console.log(`Sender: ${msg.author.username}, Message: ${msg.content}`);
+    console.log(`Sender: ${msg.author?.username}, Message: ${msg.content}`);
     console.log(`Error: ${error}`);
   }
 });
 
 client.on("message", (msg) => {
-
   try {
     const isBot = msg.author.bot;
     const id = msg.author.id;
@@ -133,10 +153,31 @@ client.on("message", (msg) => {
       if (messageString.includes("lantas")) {
         msg.channel.send(`Jangan lantas mulu <@${id}> bangsat :rage:`);
       }
+
+      if (messageString.includes('/marahin') && msg.mentions.members && msg.mentions.members.size > 0) {
+        for (const memberId of msg.mentions.members.map((x) => x.id)) {
+          msg.channel.send(generateRandomInsult(memberId))
+        }
+      }
     }
   } catch (error) {
     console.log(`Sender: ${msg.author.username}, Message: ${msg.content}`);
     console.log(`Error: ${error}`);
   }
 });
+
+cron.schedule('0 0 18 * * *', sendBirthday);
+
 client.login(process.env.TOKEN_MENANGOS);
+
+const generateRandomInsult = (id) => {
+  const insults = [
+    'OI KAK <@{id}> AKU MARAH NIH :rage::rage::rage::rage:',
+    'HEI ANDA <@{id}> APA TIDAK MAU DITAMPAR DOKTER :angry::angry::angry::angry:',
+    'KAMU MAUNYA DISUMPAHIN JOMBLO TERUS YA <@{id}>? :angel::angel::angel::angel:',
+    '<@{id}> BELOM PERNAH KELILIPAN STETOSKOP YA? :see_no_evil::see_no_evil::see_no_evil::see_no_evil:',
+    'ASAL KAMU TAU <@{id}> TETE KIMIHIME AJA MASIH LEBIH GEDE DARIPADA OTAK KAMU :speak_no_evil::speak_no_evil::speak_no_evil::speak_no_evil:',
+  ];
+  const rand = Math.round(Math.random() * 5);
+  return insults[rand].replace('{id}', id);
+}
